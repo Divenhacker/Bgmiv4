d280ed6f283e5ade3b879f794bd71eb69a67dd10
Diven <Divenmail07@gmail.com>
2024-09-01 01:09:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7255048090:AAFpMpH9KDNrr3BpnlAlraE5c2UkqhlSY5w")
+BOT_TOKEN=("7305027490:AAEJ5RJHSIvGcFFiSw55cMZWvZ5GYi45_4A")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("")
+OWNER_USERNAME=("LYXNU7")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_ID=("")
+ADMIN_ID=("7347112176")
 
 
 
